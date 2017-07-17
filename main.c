a3a1e4f802ce888f0fc601a6d8a9054f4118e48b
Unknown <leesak2004@naver.com>
2017-07-17 19:16:16 +0900

    Add print data Make shape of print

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include <pcap.h>
 #include <stdio.h>
 #include <net/ethernet.h>
-//#include <netinet/ip.h>
-//#include <netinet/in.h>
 
 int main(int argc, char *argv[])
 {
@@ -52,7 +50,8 @@ int main(int argc, char *argv[])
         if(res == 1)
         {
           p_analyze = (struct ether_header *) pkt_data;
-          printf("Source MAC Address : ");
+          printf("====================Source====================\n");
+          printf("MAC Address : ");
           for(i = 0; i < ETH_ALEN; i ++)
           {
             printf("%02x",*((p_analyze->ether_shost)+i));
@@ -60,7 +59,13 @@ int main(int argc, char *argv[])
                 printf(":");
           }
           printf("\n");
-          printf("Destination MAC Address : ");
+          printf("IP : ");
+          for(i = 26; i < 30; i ++)
+              printf("%d.",*(pkt_data+i));
+          printf("\b \n");
+          printf("Port : %d\n",*(pkt_data+34)+*(pkt_data+35));
+          printf("==================Destination=================\n");
+          printf("MAC Address : ");
           for(i = 0; i < ETH_ALEN; i ++)
           {
             printf("%02x",*((p_analyze->ether_dhost)+i));
@@ -68,19 +73,24 @@ int main(int argc, char *argv[])
                 printf(":");
           }
           printf("\n");
-          printf("Packet RAW :");
-          printf("Source IP : ");
-          for(i = 26; i < 30; i ++)
-              printf("%d.",*(pkt_data+i));
-          printf("\b \n");
-          printf("Destination IP : ");
+          printf("IP : ");
           for(i = 30; i < 34; i ++)
               printf("%d.",*(pkt_data+i));
           printf("\b \n");
+          printf("Port : %d\n",*(pkt_data+36)+*(pkt_data+37));
+          printf("=====================Data=====================\n");
+          printf("Data(Hex) : ");
+          for(i = 54; *(pkt_data+i) != NULL; i ++)
+          {
+            printf("%X",*(pkt_data+i));
+          }
           printf("\n");
-
-          printf("Source Port : %d\n",*(pkt_data+34)+*(pkt_data+35));
-          printf("Destination Port : %d\n",*(pkt_data+36)+*(pkt_data+37));
+          printf("Data(Char) : ");
+          for(i = 54; *(pkt_data+i) != NULL; i ++)
+          {
+            printf("%c",*(pkt_data+i));
+          }
+          printf("\n\n");
         }
     }
     pcap_close(handle);
