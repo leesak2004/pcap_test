99c7bbaa639d38d33fd49e402469e8e7447596f7
leesak2004 <leesak2004@naver.com>
2017-07-23 09:06:15 -0400

    Upgrade pcap_test

--- main.c
+++ main.c
@@ -1,40 +1,37 @@
 #include <pcap.h>
 #include <stdio.h>
 #include <net/ethernet.h>
-//#include <netinet/ip.h>
-//#include <netinet/in.h>
+#include <netinet/ip.h>
 
 int main(int argc, char *argv[])
 {
     int i;
-    pcap_t *handle;			/* Session handle */
+    int res;
+    int dataAddr;
     char *dev;			/* The device to sniff on */
     char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
-    struct bpf_program fp;		/* The compiled filter */
     char filter_exp[] = "port 80";	/* The filter expression */
+    struct bpf_program fp;		/* The compiled filter */
+    struct pcap_pkthdr *header;	/* The header that pcap gives us */
+    struct ether_header *p_analyze;
+    struct iphdr *ip_header;
+    struct tcphdr *tcp_header;
     bpf_u_int32 mask;		/* Our netmask */
     bpf_u_int32 net;		/* Our IP */
-    struct pcap_pkthdr *header;	/* The header that pcap gives us */
+    pcap_t *handle;			/* Session handle */
     const u_char *pkt_data;
-    struct ether_header *p_analyze;
-    int res;
 
-    /* Define the device */
-    dev = pcap_lookupdev(errbuf);
-    if (dev == NULL) {
-        fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-        return(2);
-    }
+
     /* Find the properties for the device */
-    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-        fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+    if (pcap_lookupnet(argv[1], &net, &mask, errbuf) == -1) {
+        fprintf(stderr, "Couldn't get netmask for device %s: %s\n", argv[1], errbuf);
         net = 0;
         mask = 0;
     }
     /* Open the session in promiscuous mode */
-    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    handle = pcap_open_live(argv[1], BUFSIZ, 1, 1000, errbuf);
     if (handle == NULL) {
-        fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+        fprintf(stderr, "Couldn't open device %s: %s\n", argv[1], errbuf);
         return(2);
     }
     /* Compile and apply the filter */
@@ -49,39 +46,57 @@ int main(int argc, char *argv[])
     while(1){
         /* Grab a packet */
         res = pcap_next_ex(handle, &header, &pkt_data);
-        if(res == 1)
-        {
-          p_analyze = (struct ether_header *) pkt_data;
-          printf("Source MAC Address : ");
-          for(i = 0; i < ETH_ALEN; i ++)
-          {
-            printf("%02x",*((p_analyze->ether_shost)+i));
-            if(i < 5)
-                printf(":");
-          }
-          printf("\n");
-          printf("Destination MAC Address : ");
-          for(i = 0; i < ETH_ALEN; i ++)
-          {
-            printf("%02x",*((p_analyze->ether_dhost)+i));
-            if(i < 5)
-                printf(":");
-          }
-          printf("\n");
-          printf("Packet RAW :");
-          printf("Source IP : ");
-          for(i = 26; i < 30; i ++)
-              printf("%d.",*(pkt_data+i));
-          printf("\b \n");
-          printf("Destination IP : ");
-          for(i = 30; i < 34; i ++)
-              printf("%d.",*(pkt_data+i));
-          printf("\b \n");
-          printf("\n");
+        p_analyze = (struct ether_header *) pkt_data;
+        ip_header = (struct iphdr *)(pkt_data+sizeof(*p_analyze));
+        tcp_header = (struct tcphdr *)(pkt_data+sizeof(p_analyze)+((ip_header->ihl)*4));
+        if(res == 1 && ntohs(p_analyze->ether_type) == ETHERTYPE_IP)
+            if(ntohs(p_analyze->ether_type) == ETHERTYPE_IP && ip_header->protocol == IPPROTO_TCP)
+            {
+                printf("ETHERTYPE IS IP! and PROTOCOL IS TCP!\n");
+                printf("====================Source====================\n");
+                printf("MAC Address : ");
+                for(i = 0; i < ETH_ALEN; i ++)
+                {
+                    printf("%02x",*((p_analyze->ether_shost)+i));
+                    if(i < 5)
+                        printf(":");
+                }
+                printf("\n");
+                printf("IP : ");
+                for(i = 26; i < 30; i ++)
+                    printf("%d.",*(pkt_data+i));
+                printf("\b \n");
+                printf("Port : %d\n",*(pkt_data+34)+*(pkt_data+35));
+                printf("==================Destination=================\n");
+                printf("MAC Address : ");
+                for(i = 0; i < ETH_ALEN; i ++)
+                {
+                    printf("%02x",*((p_analyze->ether_dhost)+i));
+                    if(i < 5)
+                        printf(":");
+                }
+                printf("\n");
+                printf("IP : ");
+                for(i = 30; i < 34; i ++)
+                    printf("%d.",*(pkt_data+i));
+                printf("\b \n");
+                printf("Port : %d\n",*(pkt_data+36)+*(pkt_data+37));
+                printf("=====================Data=====================\n");
+                dataAddr = sizeof(p_analyze)+sizeof(tcp_header)+sizeof(ip_header);
+                printf("Data(Hex) : ");
+                for(i = dataAddr; i < header->len; i ++)
+                {
+                    printf("%X",*(pkt_data+i));
+                }
+                printf("\n");
+                printf("Data(Char) : ");
 
-          printf("Source Port : %d\n",*(pkt_data+34)+*(pkt_data+35));
-          printf("Destination Port : %d\n",*(pkt_data+36)+*(pkt_data+37));
-        }
+                for(i = dataAddr; i < header->len;  i ++)
+                {
+                    printf("%c",*(pkt_data+i));
+                }
+                printf("\n\n");
+            }
     }
     pcap_close(handle);
     return(0);
